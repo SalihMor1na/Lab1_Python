ed96f8ac8bb7164e2020bc402cbd93998f19b795
Salih <salihmorina555@gmail.com>
2025-02-21 20:51:57 +0100

    Lägger till funktion för att plotta frekvenser av DNA-baser

--- Lab1.py
+++ Lab1.py
@@ -26,3 +26,14 @@ def count_dna_bases(sequence):
         if letter in dna_count:
             dna_count[letter] += 1
     return dna_count
+
+def plot_dna_frequencies(dna_count, sequence_id):
+    letters = list(dna_count.keys())
+    values = list(dna_count.values())
+    
+    # Skapa stapeldiagram
+    plt.bar(letters, values, color=["blue", "red", "green", "orange"])
+    plt.xlabel("DNA Baser")
+    plt.ylabel("Frekvens")
+    plt.title(f"Frekvens av DNA-baser - {sequence_id}")
+    plt.show()
