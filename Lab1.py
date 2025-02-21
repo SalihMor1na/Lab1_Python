894734d6e52ee909b33a2d128cec0b7647d98280
Salih <salihmorina555@gmail.com>
2025-02-21 20:34:48 +0100

    Lägger till funktion för att läsa DNA-fil och spara sekvenser

--- Lab1.py
+++ Lab1.py
@@ -1,4 +1,20 @@
-print('test')
-print('test')
-print('test')
-print('test')
+import matplotlib.pyplot as plt
+
+# Läs in DNA-fil
+def read_dna_file(filename):
+    with open(filename, "r") as file:
+        lines = file.readlines()
+    
+    sequences = {}
+    current_id = ""
+    
+    # Bearbeta varje rad i filen
+    for line in lines:
+        line = line.strip()
+        if line.startswith(">"):  # Identifier
+            current_id = line[1:]
+            sequences[current_id] = ""
+        else:  # DNA-sekvens
+            sequences[current_id] += line.lower()  # Gör sekvensen case-insensitive
+    
+    return sequences
