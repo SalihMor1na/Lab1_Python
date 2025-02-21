33d0d48ab80b39c101d25b0e13ec486c06f94638
Salih <salihmorina555@gmail.com>
2025-02-21 20:45:45 +0100

    Lägger till funktion för att räkna baser i DNA-sekvens

--- Lab1.py
+++ Lab1.py
@@ -18,3 +18,11 @@ def read_dna_file(filename):
             sequences[current_id] += line.lower()  # Gör sekvensen case-insensitive
     
     return sequences
+
+# Räkna baser i DNA-sekvensen
+def count_dna_bases(sequence):
+    dna_count = {"a": 0, "t": 0, "c": 0, "g": 0}
+    for letter in sequence:
+        if letter in dna_count:
+            dna_count[letter] += 1
+    return dna_count
